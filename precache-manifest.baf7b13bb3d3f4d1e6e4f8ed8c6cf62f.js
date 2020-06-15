79181eded4bfc88c0c2adead50dd81a045cbde14
Ashraful Dowla <ashrafuldowlaunited532@gmail.com>
2020-06-15 20:09:38 +0600

    Updates

---
+++ precache-manifest.baf7b13bb3d3f4d1e6e4f8ed8c6cf62f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee250c0f42492184e174c030b5886b25",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1afafa24759d3b806771",
+    "url": "/monsters-rolodex/static/css/main.3cfcc363.chunk.css"
+  },
+  {
+    "revision": "a346e020053f88bd04de",
+    "url": "/monsters-rolodex/static/js/2.c97cc4c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.c97cc4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1afafa24759d3b806771",
+    "url": "/monsters-rolodex/static/js/main.5e77aeea.chunk.js"
+  },
+  {
+    "revision": "0d227ad60af997424345",
+    "url": "/monsters-rolodex/static/js/runtime-main.95ee6a15.js"
+  }
+]);
